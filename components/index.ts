2563de75f3e58a1e5f6334b3b8094161d5ec252f
Guilherme Acosta <gw.acosta1991@gmail.com>
2025-11-24 19:28:40 -0300

    Atualização do arquivo de export de components

--- components/index.ts
+++ components/index.ts
@@ -1,20 +1,7 @@
-/**
- * Índice de exportação dos componentes
- * Centraliza as exportações de todos os componentes customizados
- * Permite importações mais limpas em outros arquivos
- * 
- * Exemplo de uso:
- * import { CustomInput, CustomButton, ConfirmModal } from '../components';
- * 
- * Em vez de:
- * import { CustomInput } from '../components/CustomInput';
- * import { CustomButton } from '../components/CustomButton';
- * import { ConfirmModal } from '../components/ConfirmModal';
- */
-
 // Exporta todos os exports do CustomInput (caminho: components/CustomInput/index.tsx)
 export * from './CustomInput';
 // Exporta todos os exports do CustomButton (caminho: components/CustomButton/index.tsx)
 export * from './CustomButton';
 // Exporta todos os exports do ConfirmModal (caminho: components/ConfirmModal/index.tsx)
 export * from './ConfirmModal';
+
